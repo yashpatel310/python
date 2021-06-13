35b3ff7b70d9edcbfc7191aadbc082cad2cccce5
Yash Patel <47658967+yashpatel310@users.noreply.github.com>
2021-06-13 11:43:47 +0530

    Create positivenumber.py

---
+++ positivenumber.py
@@ -0,0 +1,13 @@
+#LIST 1
+list1  = [12,-7,5,64,-14]
+for i in list1:
+    if i > 0:
+        print(i, end=" ")
+
+#LIST 2
+list2 = [12,14,-95,3]
+new_list = []
+for i in list2:
+    if i > 0:
+        new_list.append(i)
+print(new_list)
