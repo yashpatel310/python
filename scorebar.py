ac961a501cc924a1eae104c32ebc1773e09d53d7
Yash Patel <47658967+yashpatel310@users.noreply.github.com>
2021-07-20 16:43:26 +0530

    Update scorebar.py

--- scorebar.py
+++ scorebar.py
@@ -1,6 +1,20 @@
-import pandas as pd 
-import numpy as np 
-import matplotlib.pyplot as plt 
-plt.style.use('seaborn')
-df_score = pd.read_csv('C:\Users\Dell\Desktop\marks')
-df_score.head()
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+  
+# creating the dataset
+data = {'1st year':70, '2nd year':75, '3rd year':85,
+        '4th year':95}
+Years = list(data.keys())
+Score = list(data.values())
+  
+fig = plt.figure(figsize = (10, 5))
+ 
+# creating the bar plot
+plt.bar(Year, Score, color ='b',
+        width = 0.4)
+ 
+plt.xlabel("Student Year")
+plt.ylabel("Student Score")
+plt.title("My score in all the cycle tests in your previous year of college")
+plt.show()
